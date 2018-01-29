43e34263b65106be4068b9cacc7f551f73f9b8fd
bgklein7 <bgklein@outlook.com>
2018-01-29 11:41:01 -0500

    step 2

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -8,5 +8,46 @@ namespace IntroToOOP
 {
     class Cat
     {
+        //I need STATES
+
+        //fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+            public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+        //I need BEHAVIORS
+
+        //Constructors
+        public Cat() 
+        {
+            //this is default constructor
+            //takes no parameters, ie nothing in parentheses
+        }
+
+        public Cat (string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        public void Eat()
+        {
+            if (isHungry)
+            {
+
+                isHungry = false;
+            }
+
+            Console.WriteLine("Is the cat hungry? " + isHungry);
+        }
     }
 }
